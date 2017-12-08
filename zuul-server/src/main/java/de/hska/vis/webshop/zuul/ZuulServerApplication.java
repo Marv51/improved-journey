215f6d8fd20c78a5b13116085637a85a2a3f29b0
Moritz Sauter <SauterMoritz@gmx.de>
2017-12-08 23:36:19 +0100

    added implementation for the zuul server

--- zuul-server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
+++ zuul-server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
@@ -2,11 +2,13 @@ package de.hska.vis.webshop.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
 }

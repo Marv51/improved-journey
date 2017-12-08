9106b2a789abdf519207b70c5c0492db307907dd
Moritz Sauter <SauterMoritz@gmx.de>
2017-12-08 23:53:41 +0100

    added the last bit of example implementation

--- User-Core/src/main/java/de/hska/vis/webshop/core/user/UserController.java
+++ User-Core/src/main/java/de/hska/vis/webshop/core/user/UserController.java
@@ -5,12 +5,15 @@ import de.hska.vis.webshop.core.user.model.impl.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     // TODO stub to show the idea
+    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
     public ResponseEntity<IUser> getUser(@PathVariable Long userId) {
         return new ResponseEntity<>(new User(), HttpStatus.OK);
     }

a80f3d239c6426febd07e6b06ef22fa6f0eaa8a9
Moritz Sauter <SauterMoritz@gmx.de>
2017-12-10 19:49:19 +0100

    implemented methods from swagger, autowiring doesn't work and not tested

--- User-Core/src/main/java/de/hska/vis/webshop/core/user/UserController.java
+++ User-Core/src/main/java/de/hska/vis/webshop/core/user/UserController.java
@@ -1,7 +1,9 @@
 package de.hska.vis.webshop.core.user;
 
-import de.hska.vis.webshop.core.user.model.IUser;
-import de.hska.vis.webshop.core.user.model.impl.User;
+import de.hska.vis.webshop.core.database.dao.IUserDAO;
+import de.hska.vis.webshop.core.database.model.IUser;
+import de.hska.vis.webshop.core.database.model.impl.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    // TODO stub to show the idea
-    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
-    public ResponseEntity<IUser> getUser(@PathVariable Long userId) {
-        return new ResponseEntity<>(new User(), HttpStatus.OK);
+    @Autowired
+    private IUserDAO dao;
+
+    @RequestMapping(value = "/users/{username}", method = RequestMethod.GET)
+    public ResponseEntity<IUser> getUser(@PathVariable String username) {
+        IUser user = dao.getUserByUsername(username);
+
+        HttpStatus code = (user == null) ? HttpStatus.NOT_FOUND
+                : HttpStatus.OK;
+        return new ResponseEntity<>(user, code);
+    }
+
+    @RequestMapping(value = "/users/id/{stringId}", method = RequestMethod.GET)
+    public ResponseEntity<IUser> getUserById(@PathVariable String stringId) {
+        HttpStatus code = HttpStatus.OK;
+        IUser user;
+        try {
+            Integer parsedId = Integer.parseInt(stringId);
+            user = dao.getObjectById(parsedId);
+            if (user == null) {
+                code = HttpStatus.NOT_FOUND;
+            }
+        } catch (NumberFormatException ex) {
+            // invalid number supplied; returning null and HTTP/400
+            user = null;
+            code = HttpStatus.BAD_REQUEST;
+        }
+        return new ResponseEntity<>(user, code);
+    }
+
+    @RequestMapping(value = "/users/", method = RequestMethod.PUT)
+    public ResponseEntity<Void> getUserById(User user) {
+        HttpStatus code;
+        if (dao.saveObject(user)) {
+            code = HttpStatus.OK;
+        } else {
+            code = HttpStatus.BAD_REQUEST;
+        }
+        return new ResponseEntity<>(code);
     }
 }

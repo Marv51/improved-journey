47567dc589460f2c77b6df024bef0cd6639e90bb
Marvin Rühe <marvin@server-fish.de>
2018-01-23 00:39:57 +0100

    temporary fix for zuul-server

--- Zuul-Server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
+++ Zuul-Server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
@@ -2,10 +2,12 @@ package de.hska.vis.webshop.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZuulServerApplication {
 
     public static void main(String[] args) {

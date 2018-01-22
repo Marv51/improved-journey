3900d607755b1fb86a130d3108f035b52788ea76
Marvin Rühe <marvin@server-fish.de>
2018-01-22 23:52:31 +0100

    redirect to hystrix dashboard

--- Hystrix-Server/src/main/java/de/hska/vis/webshop/hystrix/HystrixDashboardApplication.java
+++ Hystrix-Server/src/main/java/de/hska/vis/webshop/hystrix/HystrixDashboardApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrixDashboard
+@RestController
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
@@ -16,4 +22,9 @@ public class HystrixDashboardApplication {
         } catch (InterruptedException ignored) {}
         SpringApplication.run(HystrixDashboardApplication.class, args);
     }
+
+    @RequestMapping("/")
+	public void home(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/hystrix");
+	}
 }

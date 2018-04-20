e370b3fc024b4ef730f49d3aa507dfacd6bd3552
Kevin <kevthevirus@yahoo.de>
2018-04-20 07:24:09 +0200

    Fixed package naming for turbine

--- Turbine/src/main/java/de/hska/vis/webshop/turbine/TurbineApplication.java
+++ Turbine/src/main/java/de/hska/vis/webshop/turbine/TurbineApplication.java
@@ -1,7 +1,8 @@
-package webshop.turbine;
+package de.hska.vis.webshop.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableTurbine
 @EnableEurekaClient
 @EnableHystrixDashboard
+@SpringBootApplication
 public class TurbineApplication {
 
 	public static void main(String[] args) {

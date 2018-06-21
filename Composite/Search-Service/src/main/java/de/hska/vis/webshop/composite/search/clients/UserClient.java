3c154aba156cfa450c1dacc430c12239a952a3e7
Moritz Sauter <sautermoritz@gmx.de>
2018-06-22 00:33:48 +0200

    removed former attempt on deserialization

--- Composite/Search-Service/src/main/java/de/hska/vis/webshop/composite/search/clients/UserClient.java
+++ Composite/Search-Service/src/main/java/de/hska/vis/webshop/composite/search/clients/UserClient.java
@@ -1,6 +1,5 @@
 package de.hska.vis.webshop.composite.search.clients;
 
-import de.hska.vis.webshop.composite.search.clients.config.UserClientConfiguration;
 import de.hska.vis.webshop.core.database.model.IUser;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(value = "user-service", configuration = UserClientConfiguration.class)
+@FeignClient(value = "user-service")
 public interface UserClient {
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     ResponseEntity<List<IUser>> getUserList();

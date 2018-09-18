1f12472a74726c6527a0ea9805e00df5f6b9b15c
Moritz Sauter <sautermoritz@gmx.de>
2018-09-18 15:20:07 +0200

    refactored file and removed useless stuff

--- FrontEnd/src/main/java/hska/iwi/eShopMaster/controller/LoginAction.java
+++ FrontEnd/src/main/java/hska/iwi/eShopMaster/controller/LoginAction.java
@@ -1,117 +1,109 @@
 package hska.iwi.eShopMaster.controller;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
 import de.hska.vis.webshop.core.database.model.IUser;
 import hska.iwi.eShopMaster.model.businessLogic.manager.UserManager;
 import hska.iwi.eShopMaster.model.businessLogic.manager.impl.UserManagerImpl;
-
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.opensymphony.xwork2.ActionContext;
-import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
 
 public class LoginAction extends ActionSupport {
 
-	private final static Logger logger = LoggerFactory.getLogger(LoginAction.class);
+    private final static Logger logger = LoggerFactory.getLogger(LoginAction.class);
 
-	/**
-     *
-     */
-	private static final long serialVersionUID = -983183915002226000L;
-	private String username = null;
-	private String password = null;
-	private String firstname;
-	private String lastname;
-	private String role;
+    private static final long serialVersionUID = -983183915002226000L;
+    private String username = null;
+    private String password = null;
+    private String firstname;
+    private String lastname;
+    private String role;
 
-	@Override
-	public String execute() throws Exception {
+    @Override
+    public String execute() throws Exception {
+        // Return string:
+        String result = "input";
 
-		// Return string:
-		String result = "input";
+        UserManager userManager = new UserManagerImpl();
 
-		UserManager myCManager = new UserManagerImpl();
-		
-		// Get user from DB:
-		IUser user = myCManager.getUserByUsername(getUsername());
-		// Does user exist?
-		if (user != null) {
-			// Is the password correct?
-			if (user.getPassword().equals(getPassword())) {
-				// Get session to save user role and login:
-				Map<String, Object> session = ActionContext.getContext().getSession();
-				
-				// Save user object in session:
-				session.put("webshop_user", user);
-				session.put("message", "");
-				firstname= user.getFirstname();
-				lastname = user.getLastname();
-				role = user.getRole().getTyp();
-				result = "success";
-			}
-			else {
-				addActionError(getText("error.password.wrong"));
-				logger.error("PASSWORD IS WRONG");
-			}
-		}
-		else {
-			addActionError(getText("error.username.wrong"));
-			logger.error("ERROR USERNAME WRONG");
-		}
-		logger.error("LOGGED IN SUCCESSFULLY");
-		return result;
-	}
-	
-	@Override
-	public void validate() {
-		if (getUsername() == null || getUsername().length() == 0) {
-			addActionError(getText("error.username.required"));
-		}
-		if (getPassword() == null || getPassword().length() == 0) {
-			addActionError(getText("error.password.required"));
-		}
-	}
+        // Get user from DB:
+        IUser user = userManager.getUserByUsername(getUsername());
+        // Does user exist?
+        if (user != null) {
+            // Is the password correct?
+            if (user.getPassword().equals(getPassword())) {
+                // Get session to save user role and login:
+                Map<String, Object> session = ActionContext.getContext().getSession();
 
-	public String getUsername() {
-		return (this.username);
-	}
+                // Save user object in session:
+                session.put("webshop_user", user);
+                session.put("message", "");
+                firstname = user.getFirstname();
+                lastname = user.getLastname();
+                role = user.getRole().getTyp();
+                result = "success";
+            } else {
+                addActionError(getText("error.password.wrong"));
+                logger.error("PASSWORD IS WRONG");
+            }
+        } else {
+            // user does not exist
+            addActionError(getText("error.username.wrong"));
+            logger.error("ERROR USERNAME WRONG");
+        }
+        logger.error("LOGGED IN SUCCESSFULLY");
+        return result;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @Override
+    public void validate() {
+        if (getUsername() == null || getUsername().length() == 0) {
+            addActionError(getText("error.username.required"));
+        }
+        if (getPassword() == null || getPassword().length() == 0) {
+            addActionError(getText("error.password.required"));
+        }
+    }
 
-	public String getPassword() {
-		return (this.password);
-	}
+    public String getUsername() {
+        return (this.username);
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getFirstname() {
-		return firstname;
-	}
+    public String getPassword() {
+        return (this.password);
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }

4d569217edcd7b9aee431092db3c11eb997cdf85
Moritz Sauter <sautermoritz@gmx.de>
2018-09-18 15:32:24 +0200

    enabled the creation of a product via wui

--- FrontEnd/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
+++ FrontEnd/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
@@ -1,62 +1,58 @@
 package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
+import de.hska.vis.webshop.core.database.model.ICategory;
 import de.hska.vis.webshop.core.database.model.IProduct;
+import de.hska.vis.webshop.core.database.model.impl.Category;
+import de.hska.vis.webshop.core.database.model.impl.Product;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import hska.iwi.eShopMaster.clients.ProductClient;
 import hska.iwi.eShopMaster.model.businessLogic.manager.ProductManager;
 import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
+import org.springframework.http.ResponseEntity;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class ProductManagerImpl implements ProductManager {
-	private ProductClient helper;
-	
-	public ProductManagerImpl() {
-		helper = Feign.builder().decoder(new ResponseEntityDecoder(new JacksonDecoder()))
-				.target(ProductClient.class, "http://zuul:8081");
-	}
+    private ProductClient helper;
 
-	public List<IProduct> getProducts() {
-		return helper.getProductList().getBody();
-	}
-	
-	public List<IProduct> getProductsForSearchValues(String searchDescription,
-			Double searchMinPrice, Double searchMaxPrice) {
-		return helper.getProductListByCriteria(searchDescription, searchMinPrice, searchMaxPrice).getBody();
-	}
+    public ProductManagerImpl() {
+        helper = Feign.builder().decoder(new ResponseEntityDecoder(new JacksonDecoder())).encoder(new JacksonEncoder())
+                .target(ProductClient.class, "http://zuul:8081");
+    }
 
-	public IProduct getProductById(int id) {
-		return helper.getProductById(id).getBody();
-	}
+    public List<IProduct> getProducts() {
+        return helper.getProductList().getBody();
+    }
 
-	
-	public int addProduct(String name, double price, int categoryId, String details) {
-		throw new UnsupportedOperationException();
-		/*int productId = -1;
-		
-		CategoryManager categoryManager = new CategoryManagerImpl();
-		ICategory category = categoryManager.getCategory(categoryId);
-		
-		if(category != null){
-			IProduct product;
-			if(details == null){
-				product = new Product(name, price, (Category)category);
-			} else{
-				product = new Product(name, price, (Category)category, details);
-			}
-			
-			helper.saveObject(product);
-			productId = product.getId();
-		}
-			 
-		return productId;
-		*/
-	}
-	
+    public List<IProduct> getProductsForSearchValues(String searchDescription,
+                                                     Integer searchMinPrice, Integer searchMaxPrice) {
+        ResponseEntity<List<IProduct>> productResponse = helper.getProductListByCriteria(searchDescription,
+                searchMinPrice, searchMaxPrice);
+        return productResponse.getBody();
+    }
 
-	public void deleteProductById(int id) {
-		helper.deleteProductById(id);
-	}
+    public IProduct getProductById(int id) {
+        return helper.getProductById(id).getBody();
+    }
 
+
+    public boolean addProduct(String name, double price, @NotNull ICategory category, String details) {
+        IProduct product;
+        if (details == null) {
+            product = new Product(name, price, (Category) category);
+        } else {
+            product = new Product(name, price, (Category) category, details);
+        }
+
+        ResponseEntity<Void> response = helper.saveProduct(product);
+        return response.getStatusCode().is2xxSuccessful();
+    }
+
+
+    public void deleteProductById(int id) {
+        helper.deleteProductById(id);
+    }
 }

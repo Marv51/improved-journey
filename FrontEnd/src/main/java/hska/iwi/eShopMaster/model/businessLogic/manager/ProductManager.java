a6b37c39e5da991ce1b3afd12b3e6548081ff9f9
Moritz Sauter <sautermoritz@gmx.de>
2018-09-18 15:26:48 +0200

    fix for nullpointer exception in the interface

--- FrontEnd/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/ProductManager.java
+++ FrontEnd/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/ProductManager.java
@@ -1,24 +1,23 @@
 package hska.iwi.eShopMaster.model.businessLogic.manager;
 
+import de.hska.vis.webshop.core.database.model.ICategory;
 import de.hska.vis.webshop.core.database.model.IProduct;
 
 import java.util.List;
 
 public interface ProductManager {
 
-	public List<IProduct> getProducts();
+    public List<IProduct> getProducts();
 
-	public IProduct getProductById(int id);
+    public IProduct getProductById(int id);
 
-	//public IProduct getProductByName(String name);
+    //public IProduct getProductByName(String name);
 
-	public int addProduct(String name, double price, int categoryId, String details);
+    public boolean addProduct(String name, double price, ICategory categoryId, String details);
 
-	public List<IProduct> getProductsForSearchValues(String searchValue, Double searchMinPrice, Double searchMaxPrice);
-	
-	//public boolean deleteProductsByCategoryId(int categoryId);
-	
+    public List<IProduct> getProductsForSearchValues(String searchValue, Integer searchMinPrice, Integer searchMaxPrice);
+
+    //public boolean deleteProductsByCategoryId(int categoryId);
+
     public void deleteProductById(int id);
-    
-	
 }

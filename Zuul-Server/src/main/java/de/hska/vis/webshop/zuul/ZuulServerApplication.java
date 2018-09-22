1afbacf891fdc80a10092416d8b5c6a1d496007f
Kevin <kevthevirus@yahoo.de>
2018-09-22 20:03:59 +0200

    Added AuthServer delivering a token; Adjusted Zuul to be a ResourceServer in addition

--- Zuul-Server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
+++ Zuul-Server/src/main/java/de/hska/vis/webshop/zuul/ZuulServerApplication.java
@@ -4,13 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ZuulServerApplication {
+@EnableResourceServer
+@Order(200)
+public class ZuulServerApplication extends ResourceServerConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZuulServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+	}
 }

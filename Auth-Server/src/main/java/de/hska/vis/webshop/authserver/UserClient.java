f7dd1c39e595c57f0c505dab69d12a1ede557afd
Moritz Sauter <sautermoritz@gmx.de>
2018-09-24 21:18:52 +0200

    WIP: AUTH NOT WORKING
    Theoretically the important stuff is implemented. The User details
    service exists as one of the trillion other classes I implemented in an
    extremly hacky way...please don't judge me.
    
    The server start fails for some reason. Apparently some ClassDefs are
    not found.
    
    tl;dr Nothing works, but all is fine :)

---
+++ Auth-Server/src/main/java/de/hska/vis/webshop/authserver/UserClient.java
@@ -0,0 +1,12 @@
+package de.hska.vis.webshop.authserver;
+
+import de.hska.vis.webshop.core.database.model.IUser;
+import feign.Param;
+import feign.RequestLine;
+import org.springframework.http.ResponseEntity;
+
+
+public interface UserClient {
+    @RequestLine("GET /users/{name}")
+    ResponseEntity<IUser> getUserByUsername(@Param("name") String name) throws UserNotExist;
+}

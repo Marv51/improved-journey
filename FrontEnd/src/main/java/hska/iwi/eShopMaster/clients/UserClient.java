62e31ca08baf39aef02580720475243850af7d81
Moritz Sauter <sautermoritz@gmx.de>
2018-10-29 12:39:00 +0100

    added register to feign client

--- FrontEnd/src/main/java/hska/iwi/eShopMaster/clients/UserClient.java
+++ FrontEnd/src/main/java/hska/iwi/eShopMaster/clients/UserClient.java
@@ -1,10 +1,13 @@
 package hska.iwi.eShopMaster.clients;
 
 import de.hska.vis.webshop.core.database.model.IUser;
+import de.hska.vis.webshop.core.database.model.impl.User;
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 import hska.iwi.eShopMaster.model.businessLogic.manager.impl.UserManagerImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -18,4 +21,8 @@ public interface UserClient {
 
     @RequestLine("GET /users/id/{id}")
     ResponseEntity<IUser> getUserByUserName(@Param("id") int id);
+
+    @RequestLine("POST /users")
+    @Headers("Content-Type: application/json")
+    ResponseEntity<Void> saveUser(@RequestBody User user);
 }
